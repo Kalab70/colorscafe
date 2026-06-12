3b3be1c2a443e785f37b22a916630446ade9ba25
Kalab70 <kalabwondimu05@gmail.com>
2026-06-13 01:33:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function filterMenu(category) {
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    if (category === 'all') {
+      card.style.display = 'block';
+    } else if (card.classList.contains(category)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
+
+window.filterMenu = filterMenu;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const search = document.getElementById('search');
+
+  search.addEventListener('keyup', () => {
+    let value = search.value.toLowerCase();
+
+    document.querySelectorAll('.card').forEach(card => {
+      let text = card.innerText.toLowerCase();
+      card.style.display = text.includes(value) ? 'block' : 'none';
+    });
+  });
+});
+
+function orderFood(name, price, image) {
+  const message =
+`🔔 New Order
+
+🍽 Order: ${name}
+💰 Price: ${price}
+🔢 Quantity: 1
+
+Please confirm my order.`;
+  window.open(
+    'https://t.me/+251926428941?text=' + encodeURIComponent(message),
+    '_blank'
+  );
+}
+function toggleMenu() {
+  var menu = document.getElementById('navMenu');
+  menu.classList.toggle('active');
+}
+
+function closeMenu() {
+  var menu = document.getElementById('navMenu');
+  menu.classList.remove('active');
+}
